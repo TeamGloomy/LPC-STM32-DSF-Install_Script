ff6576899d0adacf8cc8e7ec90b2cc78253496a7
P-C-R <47773180+P-C-R@users.noreply.github.com>
2020-09-18 16:50:03 +0200

    Update RRF_LPC_3_2.sh

--- RRF_LPC_3_2.sh
+++ RRF_LPC_3_2.sh
@@ -21,7 +21,7 @@ echo "-----Stop DCS and DWS---this will only works on the DuetPi with GUI-----"
 sudo systemctl stop duetcontrolserver
 sudo systemctl stop duetwebserver
 echo "-----Both stoped-----"
-sudo sed -i -e 's/8096/4096/g' /opt/dsf/conf/config.json
+sudo sed -i -e 's/8192/4096/g' /opt/dsf/conf/config.json
 sudo sed -i -e 's/"SpiTransferMode": 0/"SpiTransferMode": 3/g' /opt/dsf/conf/config.json
 echo "-----CHANGES MADE-----"
 sudo systemctl start duetcontrolserver && sudo systemctl enable duetcontrolserver
