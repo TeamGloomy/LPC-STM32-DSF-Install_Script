722b8ed300026dddeb3191fe7f4ff2368cb23543
Jay S <github@jayuk.org>
2020-12-06 14:11:08 +0000

    Beta 4 scripts

---
+++ RRF_LPC_3_2_Beta_4.sh
@@ -0,0 +1,46 @@
+echo "-----This will install the LPC Version of DSF-----"
+echo "-----Update and upgrade the PI-----"
+cd ~
+sudo apt-mark hold \
+    duetsoftwareframework \
+    duetcontrolserver \
+    duetruntime \
+    duetsd \
+    duetsoftwareframework \
+    duettools \
+    duetwebcontrol \
+    duetwebserver \
+    reprapfirmware
+sudo apt-get -q update && sudo apt-get -y upgrade
+echo "-----Upgrade and Update finished-----"
+echo "-----Swichting to the unstable branch-----"
+wget -q https://pkg.duet3d.com/duet3d.gpg
+wget -q https://pkg.duet3d.com/duet3d-unstable.list
+sudo mv duet3d.gpg /etc/apt/trusted.gpg.d/
+sudo mv duet3d-unstable.list /etc/apt/sources.list.d/duet3d-unstable.list
+sudo chown root:root /etc/apt/trusted.gpg.d/duet3d.gpg
+sudo chown root:root /etc/apt/sources.list.d/duet3d-unstable.list
+sudo rm /etc/apt/sources.list.d/duet3d.list
+echo "-----Switching finished-----"
+sudo apt -q update 
+sudo sudo apt install \
+    duetsoftwareframework=3.2.0-beta4 \
+    duetcontrolserver=3.2.0-beta4 \
+    duetruntime=3.2.0-beta4 \
+    duetsd=1.1.0 \
+    duetsoftwareframework=3.2.0-beta4 \
+    duettools=3.2.0-beta4 \
+    duetwebcontrol=3.2.0-beta4 \
+    duetwebserver=3.2.0-beta4 \
+    reprapfirmware=3.2.0-beta4-1 \
+    --allow-downgrades
+echo "-----Stop DCS and DWS---"
+sudo systemctl stop duetcontrolserver
+sudo systemctl stop duetwebserver
+echo "-----Both stoped-----"
+sudo sed -i -e 's/"SpiBufferSize": 8192/"SpiBufferSize": 3072/g' /opt/dsf/conf/config.json
+sudo sed -i -e 's/"SpiTransferMode": 0/"SpiTransferMode": 3/g' /opt/dsf/conf/config.json
+echo "-----CHANGES MADE-----"
+sudo systemctl start duetcontrolserver && sudo systemctl enable duetcontrolserver
+sudo systemctl start duetwebserver && sudo systemctl enable duetwebserver
+echo "-----DCS and DWS started-----"
