73079c6769dc669064bcf46eecae67f998752b3b
Jay <github@jayuk.org>
2021-05-14 08:41:59 +0100

    3.3-RC2 files

---
+++ RRF_STM_3_3_RC_2.sh
@@ -0,0 +1,52 @@
+echo "-----This will install the STM Version of DSF for 3.3-RC2-----"
+echo "-----Update and upgrade the PI-----"
+cd ~
+sudo apt-mark hold \
+    duetsoftwareframework \
+    duetcontrolserver \
+    duetruntime \
+    duetsd \
+    duettools \
+    duetwebcontrol \
+    duetwebserver \
+    reprapfirmware
+sudo apt-get -q update && sudo apt-get -y upgrade
+echo "-----Upgrade and Update finished-----"
+echo "-----Switching to the unstable branch-----"
+sudo apt-mark unhold \
+    duetsoftwareframework \
+    duetcontrolserver \
+    duetruntime \
+    duetsd \
+    duettools \
+    duetwebcontrol \
+    duetwebserver \
+    reprapfirmware
+wget -q https://pkg.duet3d.com/duet3d.gpg
+wget -q https://pkg.duet3d.com/duet3d-unstable.list
+sudo mv duet3d.gpg /etc/apt/trusted.gpg.d/
+sudo mv duet3d-unstable.list /etc/apt/sources.list.d/duet3d-unstable.list
+sudo chown root:root /etc/apt/trusted.gpg.d/duet3d.gpg
+sudo chown root:root /etc/apt/sources.list.d/duet3d-unstable.list
+sudo rm /etc/apt/sources.list.d/duet3d.list
+echo "-----Switching finished-----"
+sudo apt -q update 
+sudo apt install \
+    duetsoftwareframework=3.3-RC2 \
+    duetcontrolserver=3.3-rc2 \
+    duetruntime=3.3-rc2 \
+    duetsd=1.1.0 \
+    duettools=3.3-rc2 \
+    duetwebcontrol=3.3-rc2 \
+    duetwebserver=3.3-rc2 \
+    reprapfirmware=3.3-rc2-1 \
+    --allow-downgrades
+sudo apt-mark hold \
+    duetsoftwareframework \
+    duetcontrolserver \
+    duetruntime \
+    duetsd \
+    duettools \
+    duetwebcontrol \
+    duetwebserver \
+    reprapfirmware
